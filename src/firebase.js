8e2bf5f4b0279eb027b4eabc9eb3581a8645b2f3
komcal <kom.sujautra@gmail.com>
2019-04-11 18:03:10 +0700

    try catch firebase

--- src/firebase.js
+++ src/firebase.js
@@ -7,30 +7,34 @@ import config from './config'
 firebase.initializeApp(config.firebase);
 
 function initializePush() {
-   const messaging = firebase.messaging();
-   messaging
-      .requestPermission()
-      .then(() => {
-         console.log("Have Permission");
-         return messaging.getToken();
-       })
-      .then(token => {
-         const currentToken = localStorage.getItem('token')
-         console.log(token)
-         if (!currentToken) {
-            firebase.database().ref('tokens').push({
-               token
-            })
-            localStorage.setItem('token', token)
-         }
-       })
-      .catch(error => {
-         if (error.code === "messaging/permission-blocked") {
-            console.log("Please Unblock Notification Request Manually");
-         } else {
-            console.log("Error Occurred", error);
-         }
-        });
+   try {
+      const messaging = firebase.messaging();
+      messaging
+         .requestPermission()
+         .then(() => {
+            console.log("Have Permission");
+            return messaging.getToken();
+         })
+         .then(token => {
+            const currentToken = localStorage.getItem('token')
+            console.log(token)
+            if (!currentToken) {
+               firebase.database().ref('tokens').push({
+                  token
+               })
+               localStorage.setItem('token', token)
+            }
+         })
+         .catch(error => {
+            if (error.code === "messaging/permission-blocked") {
+               console.log("Please Unblock Notification Request Manually");
+            } else {
+               console.log("Error Occurred", error);
+            }
+         });
+   } catch(err) {
+      console.log(err)
+   }
 }
 
 export {
