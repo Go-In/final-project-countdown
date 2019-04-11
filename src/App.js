57a57affbac5fcc18a90e0e2b5dae18d80d120a9
eveem <even_narak@hotmail.coml>
2019-04-11 13:51:32 +0700

    added counter reder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    day: "00",
+    hour: "00",
+    minute: "00",
+    second: "00",
+    countDownDate: new Date("Apr 24, 2019 00:00:00").getTime(),
+    distance: 0
+  }
+  
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      console.log("EIEI");
+      let distance = this.state.countDownDate - new Date().getTime();
+      let dayt = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hourt = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutet =  Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let secondt = Math.floor((distance % (1000 * 60)) / 1000);
+        this.setState ({
+          day: dayt < 10 ? "0" + dayt : dayt,
+          hour: hourt < 10 ? "0" + hourt : hourt,
+          minute: minutet < 10 ? "0" + minutet : minutet,
+          second: secondt < 10 ? "0" + secondt : secondt
+        });
+    }, 1000);
+    console.log(this.state.day, this.state.hour, this.state.minute, this.state.second);
+  }
+
   render() {
+    console.log(this.state.day, this.state.hour, this.state.minute, this.state.second);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <video autoplay muted loop id="myVideo">
+          {/* <source src="http://frolovoleg.ru/videos/abstract_bg.mp4" type="video/mp4"/> */}
+        </video>
+        <div className="timer">
+          <div className="days">
+            <div id="days">{this.state.day}</div>
+            <div className="label">Days</div>
+          </div>
+          <div className="seperator">
+            <div className="dot"></div>
+            <div className="dot"></div>
+          </div>
+          <div className="hours">
+            <div id="hours">{this.state.hour}</div>
+            <div className="label">Hours</div>
+          </div>
+          <div className="seperator">
+            <div className="dot"></div>
+            <div className="dot"></div>
+          </div>
+          <div className="minutes">
+            <div id="minutes">{this.state.minute}</div>
+            <div className="label">Minutes</div>
+          </div>
+          <div className="seperator">
+            <div className="dot"></div>
+            <div className="dot"></div>
+          </div>
+          <div className="seconds">
+            <div id="seconds">{this.state.second}</div>
+            <div className="label">Seconds</div>
+          </div>
+        </div>
       </div>
     );
   }
